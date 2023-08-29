42a1a4aeef492ff27c71a34b9accabccc2850ed0
Shaun Curtis <shaun@coldelm.com>
2023-08-29 22:07:15 +0100

    Update and intital release

---
+++ Source/Libraries/Blazr.OneWayStreet.Diode/Package.md
@@ -0,0 +1,3 @@
+This package provides the integration of Blazr Diode with the Blazr One Way Street Command Data Pipeline
+
+See the Repo -  https://github.com/ShaunCurtis/Blazr.OneWayStreet.Diode
--- Source/Libraries/Blazr.OneWayStreet/Usings.cs
+++
@@ -1,9 +0,0 @@
-﻿global using Blazr.OneWayStreet.Core;
-global using Blazr.Core;
-
-global using System.Diagnostics.CodeAnalysis;
-global using System.Text.Json;
-global using System.Linq.Expressions;
-global using Microsoft.EntityFrameworkCore;
-
-global using Microsoft.Extensions.DependencyInjection;
